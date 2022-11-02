bd81393a43905e42214b763cc26e889863367230
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-02 20:31:43 -0300

    FIX database relation unique togeter (username task id)

--- api/models.py
+++ api/models.py
@@ -8,6 +8,9 @@ class Todo(models.Model):
     is_closed = models.BooleanField(default=False)
     name = models.CharField(max_length=50, blank=False, null=False)
     content = models.TextField()
-    task_id = models.CharField(max_length=500, blank=False, null=False, unique=True)
-    username = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    task_id = models.CharField(max_length=500, blank=False, null=False)
+    username = models.CharField(max_length=100, blank=False, null=False)
     reopen_date = models.DateTimeField(null=True)
+
+    class Meta:
+        unique_together = ('username', 'task_id')
