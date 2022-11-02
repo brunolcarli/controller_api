478d54c519f3c26311dc17b2215e628eb5bda46b
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-02 20:03:39 -0300

    ADD reopen date field to Todo model

--- api/models.py
+++ api/models.py
@@ -10,3 +10,4 @@ class Todo(models.Model):
     content = models.TextField()
     task_id = models.CharField(max_length=500, blank=False, null=False, unique=True)
     username = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    reopen_date = models.DateTimeField(null=True)
