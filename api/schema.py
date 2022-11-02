2a78d3fed8ec94cd183c7ea067062801ea03ce25
Bruno L. Carli <brunolcarli@gmail.com>
2022-11-02 20:03:55 -0300

    ADD task close/reopen/update mutations

--- api/schema.py
+++ api/schema.py
@@ -38,6 +38,7 @@ class CreateTask(graphene.relay.ClientIDMutation):
     task = graphene.Field(Task)
 
     class Input:
+        task_id = graphene.ID(required=True)
         username = graphene.String(required=True)
         name = graphene.String(required=True)
         creation_date = graphene.DateTime(required=True)
@@ -45,9 +46,101 @@ class CreateTask(graphene.relay.ClientIDMutation):
 
     def mutate_and_get_payload(self, info, **kwargs):
         task = Todo.objects.create(**kwargs)
+        task.save()
 
         return CreateTask(task)
 
 
+class CloseTask(graphene.relay.ClientIDMutation):
+    task = graphene.Field(Task)
+
+    class Input:
+        task_id = graphene.ID(required=True)
+        username = graphene.String(required=True)
+        closing_date = graphene.DateTime(required=True)
+
+    def mutate_and_get_payload(self, info, **kwargs):
+        try:
+            task = Todo.objects.get(
+                username=kwargs['username'],
+                task_id=kwargs['task_id']
+            )
+        except Todo.DoesNotExist:
+            raise Exception('Task not found on database!')
+
+        if task.is_closed:
+            raise Exception('Task is already closed!')
+
+        task.closing_date = kwargs['closing_date']
+        task.is_closed = True
+        task.save()
+
+        return CloseTask(task)
+
+
+class ReopenTask(graphene.relay.ClientIDMutation):
+    task = graphene.Field(Task)
+
+    class Input:
+        username = graphene.String(required=True)
+        task_id = graphene.ID(required=True)
+        reopen_date = graphene.DateTime(required=True)
+
+    def mutate_and_get_payload(self, info, **kwargs):
+        try:
+            task = Todo.objects.get(
+                username=kwargs['username'],
+                task_id=kwargs['task_id']
+            )
+        except Todo.DoesNotExist:
+            raise Exception('Task not found on database!')
+
+        if not task.is_closed:
+            raise Exception('Cannot reopen an open task!')
+
+        task.is_closed = False
+        task.reopen_date = kwargs['reopen_date']
+        task.save()
+
+        return ReopenTask(task)
+
+
+class UpdateTask(graphene.relay.ClientIDMutation):
+    task = graphene.Field(Task)
+
+    class Input:
+        username = graphene.String(required=True)
+        task_id = graphene.ID(required=True)
+        name = graphene.String()
+        content = graphene.String()
+
+    def mutate_and_get_payload(self, info, **kwargs):
+        name = kwargs.get('name')
+        content = kwargs.get('content')
+
+        try:
+            task = Todo.objects.get(
+                username=kwargs['username'],
+                task_id=kwargs['task_id']
+            )
+        except Todo.DoesNotExist:
+            raise Exception('Task not found on database!')
+
+        if task.is_closed:
+            raise Exception('Cannot update an closed task!')
+
+        if name:
+            task.name = name
+        if content:
+            task.content = content
+
+        task.save()
+
+        return UpdateTask(task)
+
+
 class Mutation:
     create_task = CreateTask.Field()
+    close_task = CloseTask.Field()
+    reopen_task = ReopenTask.Field()
+    update_task = UpdateTask.Field()
